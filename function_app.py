a0db2f4f6467ebf6dc2b7a4f5bdc4391ef3a0636
Shreya Batra <shbatr@microsoft.com>
2022-06-07 11:44:35 -0500

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,3 +1,12 @@
 import azure.functions as func
 
 app = func.FunctionApp()
+
+# Learn more here: 
+
+# Comment out the following to test the HTTP Trigger function
+
+# @app.function_name(name="HttpTrigger1")
+# @app.route(route="hello") # HTTP Trigger
+# def test_function(req: func.HttpRequest) -> func.HttpResponse:
+     # return func.HttpResponse("HttpTrigger1 function processed a request!!!")
